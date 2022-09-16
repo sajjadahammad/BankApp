9ec8195a26fb33265820fc0430840fbaa4f6cc1a
sajjadahammed <sajjad08@protonmail.com>
2022-09-16 12:31:17 +0530

    16-09-2022

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  ent="Enter your account number"
+  stat="Welcome to Axis Bank"
+  accountNumber:number=0
+  passWord:string=''
+  dataBase:any={
+    101:{acno:101,uname:"sajjad",password:1000,balance:50000}
+  }
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  accountNumberChange(event:any){
+    this.accountNumber=event.target.value
+    console.log("this.accountNumberChange",this.accountNumberChange)
+  }
+
+  passwordChange(event:any){
+    this.passWord=event.target.value
+    console.log("this.passWord",)
+  }
+   
+  login(){
+    var acno = this.accountNumber
+    var pswd = this.passWord
+
+    if(acno in this.dataBase){
+      if(pswd ==this.dataBase[acno]["password"]){
+        alert("login success")
+      }else{
+        alert("Incorrect password Login failed")
+      }
+    }
+   else{
+    alert("Incorrect Account Number")
+  }
+}
+
+}
