91ebe6bfc1c6d17feafe05eb0fec618180d8ae0a
sajjadahammed <sajjad08@protonmail.com>
2022-09-17 12:43:34 +0530

    17-09-2022

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   ent="Enter your account number"
   stat="Welcome to Axis Bank"
+  regUserName:string=''
+  regAccountNumber:number=0
+  regPassword:string=''
   accountNumber:number=0
   passWord:string=''
   dataBase:any={
-    101:{acno:101,uname:"sajjad",password:1000,balance:50000}
+   101:[{acno:101,uname:"sajjad",password:1000,balance:50000}]
   }
 
 
@@ -22,12 +25,12 @@ export class LoginComponent implements OnInit {
 
   accountNumberChange(event:any){
     this.accountNumber=event.target.value
-    console.log("this.accountNumberChange",this.accountNumberChange)
+    console.log("this.accountNumberChange",this.accountNumber)
   }
 
   passwordChange(event:any){
     this.passWord=event.target.value
-    console.log("this.passWord",)
+    console.log("this.passWord",this.passWord)
   }
    
   login(){
@@ -44,6 +47,31 @@ export class LoginComponent implements OnInit {
    else{
     alert("Incorrect Account Number")
   }
+}
+
+userNameChange(event:any){
+  this.regUserName=event.target.value
+  console.log("Reguname: ",this.regUserName)
+}
+
+regAcntNumChange(event:any){
+  this.regAccountNumber=event.target.value
+  console.log("Regaccntnum: ",this.regAccountNumber)
+}
+
+regPswdChange(event:any){
+ this.regPassword=event.target.value
+ console.log("Regpswd: ",this.regPassword)
+}
+
+signUp(){
+  var regUname = this.regUserName
+  var regAcntNumber = this.regAccountNumber
+  var regpswd =this.regPassword
+
+
+  console.log("details",this.dataBase)
+
 }
 
 }
